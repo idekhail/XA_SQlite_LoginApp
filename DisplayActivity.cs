0ead47f4758c615159824671509c75de7f071361
Ibrahim AlDekhail <33824044+idekhail@users.noreply.github.com>
2022-10-24 10:56:37 +0300

    Add files via upload

--- DisplayActivity.cs
+++ DisplayActivity.cs
@@ -7,69 +7,50 @@ using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 
-using Xamarin.Essentials;
-
 namespace XA_SQlite_LoginApp
 {
-    [Activity(Label = "DisplayActivity")]
+    [Activity(Label = "ScreenActivity")]
     public class DisplayActivity : Activity
-    {        
+    {
+        
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             SetContentView(Resource.Layout.activity_display);
             TextView show = FindViewById<TextView>(Resource.Id.show);
-
-            Button logout = FindViewById<Button>(Resource.Id.logout);
+            Button back = FindViewById<Button>(Resource.Id.back);
             Button allUsers = FindViewById<Button>(Resource.Id.allUsers);
             Button call = FindViewById<Button>(Resource.Id.call);
-            Button sendemail = FindViewById<Button>(Resource.Id.sendemail);
 
             MySqliteDB sq = new MySqliteDB();
             MySqliteDB.Users user = new MySqliteDB.Users();
             user = JsonConvert.DeserializeObject<MySqliteDB.Users>(Intent.GetStringExtra("user"));
+            var address = sq.GetAddress(user.Username);
 
             try {
-                show.Text = user.UId + "\t\t\t" + user.Username + "\t\t\t" + user.Password + "\t\t\t" + user.Mobile + "\t\t\t" + user.Email;
+                show.Text = user.UId + "\t\t\t" + user.Username + "\t\t\t" + user.Password + "\t\t\t" + user.Mobile
+                     + "\t\t\t" + address.City + "\t\t\t" + address.HomeNu;
             }catch(Exception e) {
                 show.Text = e.Message;
-            }      
-            
+            }                
             allUsers.Click += delegate {
                 try {
-                    show.Text = sq.RetrieveAllUser();
+                    show.Text = sq.GetAllUser();
                 } catch (Exception e) {
                     show.Text = e.Message;
                 }
             }; 
-
             call.Click += delegate {
                 var url = Android.Net.Uri.Parse("tel:" + user.Mobile);
                 var intent = new Intent(Intent.ActionDial, url);
                 StartActivity(intent);
             };      
-
-            logout.Click += delegate {
+            back.Click += delegate {
                 Intent i = new Intent(this, typeof(LoginActivity));
                 StartActivity(i);
             };
 
-            sendemail.Click += delegate
-            {
-                var email = new Intent(Android.Content.Intent.ActionSend);
-                email.PutExtra(Android.Content.Intent.ExtraEmail, new string[] {
-                    user.Email,
-                    "idekhail@tvtc.gov.sa"
-                });
-                //email.PutExtra(Android.Content.Intent.ExtraCc, new string[] {
-                //    "Your Email from db"
-                //});
-                email.PutExtra(Android.Content.Intent.ExtraSubject, "Midterm2");
-                email.PutExtra(Android.Content.Intent.ExtraText, "Final Lab Exam for Email Xamarin!");
-                email.SetType("message/rfc822");
-                StartActivity(email);
-            };
         }
     }
 }
